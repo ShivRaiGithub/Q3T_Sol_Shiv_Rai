d5b9b14fafde8fb6d5194748f0f5f34c97e31e4d
Shiv Rai <sshaktirai@gmail.com>
2024-08-24 17:49:47 +0530

    adjusted code

--- blink-choose-starter/src/app/page.tsx
+++ blink-choose-starter/src/app/page.tsx
@@ -12,8 +12,9 @@ export default function Home() {
 
       {/* Content */}
       <h1 className="text-5xl font-bold mb-8 text-center z-10">Solana Blink Starter Pokémon</h1>
+      <h1 className="text-1xl text-center z-10 hover:underline"><a href="https://dial.to/?action=solana-action:https://choose-starter-lac.vercel.app/api/choose">https://dial.to/?action=solana-action:https://choose-starter-lac.vercel.app/api/choose</a></h1>
 
-      <div className="flex flex-col items-center gap-6 w-full max-w-7xl px-4 py-12 mx-auto z-10">
+      <div className="flex flex-col items-center gap-6 w-full max-w-7xl px-4 py-5 mx-auto z-10">
         <div className="flex flex-col items-center justify-center bg-white rounded-lg p-8 shadow-md text-black">
           <div className="relative w-[33.33vw] h-[25vh]">
             <Image
@@ -28,7 +29,7 @@ export default function Home() {
       </div>
 
       <section className="mt-12 z-10 px-4">
-        <h2 className="text-3xl font-bold mb-4">About</h2>
+        <h2 className="text-3xl font-bold m-4">About</h2>
         <p className="mb-4">
           This is a Solana-based project that allows users to choose starter Pokémon as NFTs.
         </p>
@@ -37,8 +38,8 @@ export default function Home() {
           <li>Each Pokémon has its own unique NFT associated with it.</li>
           <li>The project uses Solana&apos;s blockchain technology to mint and manage these NFTs.</li>
           <li>
-            Users can interact with the project by visiting:
-            <a href="https://dial.to" className="text-blue-500 hover:underline">dial.to</a> and entering the site&apos;s URL + &quot;/api/choose&quot;.
+            Users can interact with the project by visiting:{" "}
+            <a href="https://dial.to/?action=solana-action:https://choose-starter-lac.vercel.app/api/choose" className="text-blue-500 hover:underline">dial.to</a>
           </li>
           <li>The project runs on the devnet network.</li>
           <li>Ensure you have sufficient funds to cover transaction fees for minting NFTs.</li>
