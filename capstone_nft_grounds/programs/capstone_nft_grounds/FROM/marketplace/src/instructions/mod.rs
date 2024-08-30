f8c4d790203753c4fc04bbbb045a3bf786aa2b54
Shiv Rai <sshaktirai@gmail.com>
2024-08-30 10:33:29 +0530

    Removed capstone_nft_grounds from Git tracking

--- capstone_nft_grounds/programs/capstone_nft_grounds/FROM/marketplace/src/instructions/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod initialize;
-pub mod listing;
-pub mod delist;
-
-pub use initialize::*;
-pub use listing::*;
-pub use delist::*;
--- capstone_nft_grounds/programs/capstone_nft_grounds/FROM/nft_stake/src/error.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[error_code]
-pub enum ErrorCode{
-    #[msg("Maximum number of stakes reached")]
-    MaxStakes,
-    #[msg("Staking has not matured")]
-    UnstakeFreezeDurationInvalid,
-}
